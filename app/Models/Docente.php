2d8cea831fd6eca530b44dabe5edf2143c86445a
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2026-02-11 14:48:43 -0500

    Add UUID support to Docente model

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-// --- MODELO DOCENTE ---
-class Docente extends Model {
+class Docente extends Model
+{
+    use HasUuids;
+
     protected $fillable = ['nombre_completo'];
-    public function programaciones(): HasMany { return $this->hasMany(ProgramacionAcademica::class); }
+
+    public function programaciones(): HasMany
+    {
+        return $this->hasMany(ProgramacionAcademica::class);
+    }
 }
